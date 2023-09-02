0a153695340ca7aca7734dbc14d09aee193ad0b8
space-mp4-exe <you@example.com>
2023-09-02 01:47:55 -0500

    done for now

--- crossProductCalc.cpp
+++ crossProductCalc.cpp
@@ -3,12 +3,14 @@
 #include <cmath>
 using namespace std;
 
+//Calculates the length, dot product, and cross product of two vetors in R^3 space
+//don't ask me why it changes from a int vector to a float vector halfway thru
 class Calc{
 	public:
-		void assignVector(vector<int>* v);//assigns the values for a 3 length int vector
+		vector<int> assignVector(vector<int> v);//assigns the values for a 3 length int vector
 		float findLength(vector<int> v); //returns length of vector 
 		float dotProduct(vector<int> v1, vector<int> v2); //returns the dot product
-		vector<int> crossProduct(vector<int> v1, vector<int> v2);//returns the cross product.
+		vector<float> crossProduct(vector<int> v1, vector<int> v2);//returns the cross product.
 		string vectorToString(vector<int> v);
 };
 
@@ -16,26 +18,21 @@ int main(){
 	Calc calc;
 	vector<int> v1(3);
 	vector<int> v2(3);
-	calc.assignVector(&v1);
-	calc.assignVector(&v2);
-	//cout << v1.at(0);
-	//cout << v2.at(0) << v2.at(1) << v2.at(2);
+	v1 = calc.assignVector(v1);
+	v2 = calc.assignVector(v2);
 
-	/*
-	cout << calc.vectorToString(v1);
-	cout << calc.vectorToString(v2);
-	
-	calc.findLength(v1);
-	calc.findLength(v2);
-	calc.dotProduct(v1, v2);
-	calc.crossProduct(v1, v2);
-	*/
+	cout <<"v1 length: "<< calc.findLength(v1) <<" or sqrt(" << pow(v1.at(0), 2) + pow(v1.at(1), 2) + pow(v1.at(2), 2) << ")" << endl;
+	cout <<"v2 length: "<< calc.findLength(v2) <<" or sqrt(" << pow(v2.at(0), 2) + pow(v2.at(1), 2) + pow(v2.at(2), 2) << ")" <<endl;
+	cout <<"dot product: " << calc.dotProduct(v1, v2) << endl;
+	vector<float> cpVector = calc.crossProduct(v1, v2);
+	cout <<"cross product: < " << cpVector[0] << ", " << cpVector[1] << ", " << cpVector[2] << " >" << endl;
 };
 
-void Calc::assignVector(vector<int>* v){
+vector<int> Calc::assignVector(vector<int> v){
 	int x = 0, y = 0, z = 0;
-	//cin >> x >> y >> z;
-	//v->assign({x, y, z});
+	cin >> x >> y >> z;
+	v.assign({x, y, z});
+	return v;
 };
 
 float Calc::findLength(vector<int> v){
@@ -44,13 +41,20 @@ float Calc::findLength(vector<int> v){
 };
 
 float Calc::dotProduct(vector<int> v1, vector<int> v2){
-	cout << -1;
-	return -1;
+	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 };
 
-vector<int> Calc::crossProduct(vector<int> v1, vector<int> v2){
-	cout << -1;
-	vector<int> v(1,0);
+vector<float> Calc::crossProduct(vector<int> v1, vector<int> v2){
+	vector<float> v;
+	float iCon;
+	float jCon;
+	float kCon;
+
+	iCon = v1[1] * v2[2] - v1[2] * v2[1];
+	jCon = v1[0] * v2[2] - v1[2] * v2[0];
+	jCon *= -1;//flip sign of j conponent because that's just what we do
+	kCon = v1[0] * v2[1] - v1[1] * v2[0];
+	v.assign({iCon, jCon, kCon});
 	return v;
 };
 
